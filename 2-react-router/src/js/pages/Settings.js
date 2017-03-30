c3492723249d30ca54382372f424b6e4449243cb
charlesEWest <charleswest33@gmail.com>
2017-03-29 22:15:24 -0500

    first commit

---
+++ 2-react-router/src/js/pages/Settings.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default class Settings extends React.Component {
+  render() {
+    console.log("Login Page");
+    return (
+      <h1>Colorist</h1>
+    );
+  }
+}
